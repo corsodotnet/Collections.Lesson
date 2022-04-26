80871cd9cb565b8a7dd8e3186dc8687ee0d5234b
Bruno Ferreira Rocha <bruno_ferreira@hotmail.it>
2022-04-26 11:50:02 +0200

    Add project files.

---
+++ Collections.Lesson/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region LIST
+            List<string> list = new List<string>() { "Bruno", "Mario" };
+            list.Add("");
+            list.AddRange(new string[] { "bruno", "Mario" });
+            list.Remove("bruno");
+            list.RemoveAll(x =>
+            {
+                if (x.StartsWith("b"))
+                    return true;
+                else
+                    return false;
+
+            });
+            list.RemoveAt(list.Count - 1);
+            #endregion
+            #region Dictionary
+
+            Dictionary<int, string> dict = new Dictionary<int, string>()
+            {
+
+                {1,"MI,BO, VE" },
+
+            };
+            var italia = dict[1];
+
+            if (dict.ContainsKey(1))
+            {
+                foreach (var item in italia) ;
+
+            }
+            string result;
+            if (dict.TryGetValue(1, out result))
+            {
+
+            }
+
+            for (int i = 0; i < dict.Count; i++)
+            {
+                Console.WriteLine("Key:{0}, Value:{1}",
+                    dict.ElementAt(i).Key,
+                    dict.ElementAt(i).Value);
+            }
+            dict.Remove(1);// _>
+
+            dict.Clear();
+            #endregion
+        }
+    }
+}
---
+++ LinqLesson/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student() { Id = 1, Name ="Bruno",Age = 40},
+                new Student() { Id = 2, Name ="Alice",Age = 20},
+                new Student() { Id = 3, Name ="Mario",Age = 25},
+                new Student() { Id = 4, Name ="Aila",Age = 20},
+                new Student() { Id = 5, Name ="Dario",Age = 40}
+            };
+
+            var query = from student in studentList group student by student.Age;
+
+            foreach (var student in query)
+            {
+                Console.WriteLine(student.Key); // gruppo
+
+                foreach (var s in student)
+                {
+                    Console.WriteLine("{0}", s.Name); //Elementi del gruppo
+                }
+            }
+
+        }
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
